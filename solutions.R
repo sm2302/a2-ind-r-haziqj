3999f54f2ee269cc0f7ea9ae2842445f32669693
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2022-10-24 19:46:57 +0800

    Complete Q2

--- solutions.R
+++ solutions.R
@@ -17,3 +17,23 @@ pareto_pdf <- function(x, alpha, beta) {
 # pareto_pdf(10, 5, 5)
 # pareto_pdf(10:15, 5, 5)  # it's vectorised too
 
+# Q2 ---------------------------------------------------------------------------
+pareto_dev <- function(alpha, beta, x) {
+  # First test for invalid parameter values
+  if (alpha <= 0 | beta <= 0)
+    stop("Parameters alpha and beta must be > 0.")
+  
+  # Return the log-likelihood
+  res <- -2 * log(alpha) - 2 * alpha * log(beta) + 
+    2 * (alpha + 1) * log(x[x > beta])  # only interested in values for which
+                                        # x > beta
+  sum(res)
+}
+
+# Some tests:
+# pareto_dev(2, 2, 2:10)
+# sum(-2 * log(pareto_pdf(x = 2:10, alpha = 2, beta = 2)))
+
+# Note: It's fine to do sum(-2 * log(pareto_pdf)), but it breaks down easily
+# because the pdf explodes with large values of alpha. Better to use log scale
+# directly.
