372e8cf927d33416f8824195387989ef66142ce0
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2022-10-24 19:46:49 +0800

    Complete Q1

--- solutions.R
+++ solutions.R
@@ -3,6 +3,17 @@ library(tidyverse)
 
 # Q1 ---------------------------------------------------------------------------
 pareto_pdf <- function(x, alpha, beta) {
-  # your code here
+  # First test for invalid parameter values
+  if (alpha <= 0 | beta <= 0)
+    stop("Parameters alpha and beta must be > 0.")
+  
+  # The Pareto pdf
+  res <- alpha * beta ^ alpha / x ^ (alpha + 1)
+  res[x < beta] <- 0  # pdf is zero when x < beta
+  return(res)
 }
+
+# Some tests:
+# pareto_pdf(10, 5, 5)
+# pareto_pdf(10:15, 5, 5)  # it's vectorised too
 
